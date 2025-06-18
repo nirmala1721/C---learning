ebdd7f39108ef4762b0d4b3bd1ebf4816a021eb6
Nirmala Bigimalla <nirmalabigimalla@gmail.com>
2025-06-18 15:33:43 +0530

    datatypes

---
+++ Data_types/datatypes.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age=20;
+    float marks=98.98;
+    char garde='a';
+    string name="xyz";
+    bool pass=true;
+
+    cout<<age<<" : age"<<endl;
+    cout<<marks<<": marks"<<endl;
+    cout<<garde<<": grade"<<endl;
+    cout<<name<<": name"<<endl;
+    cout<<pass<<": pass"<<endl;
+    return 0;
+
+
+}
